2fcfea58c604749616b645912583d07c5757a720
Pablo Sebastián Caballero <pablocaballero1295@gmail.com>
2024-09-25 13:32:13 +0100

    Updated navbar and Readme file

--- src/components/NavBarButton/NavBarButton.tsx
+++ src/components/NavBarButton/NavBarButton.tsx
@@ -1,5 +1,5 @@
 import styles from "./NavBarButton.module.css"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 interface NavBarButtonProps {
   text: string
@@ -7,8 +7,23 @@ interface NavBarButtonProps {
 }
 
 export const NavBarButton = ({ text, to }: NavBarButtonProps) => {
+  const location = useLocation()
+
+  const isActiveRoute = (route: string) => {
+    if (location.pathname == route) {
+      console.log(location.pathname)
+      console.log(route)
+      return true
+    }
+  }
+
   return (
-    <Link to={to} className={styles.navbar_button}>
+    <Link
+      to={to}
+      className={`${styles.navbar_button} ${
+        isActiveRoute(to) ? styles.active : ""
+      }`}
+    >
       {text}
     </Link>
   )
