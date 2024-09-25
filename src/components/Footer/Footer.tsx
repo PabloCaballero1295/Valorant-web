8480c8ea346862ed68d2b617c61248705771ad83
Pablo Sebastián Caballero <pablocaballero1295@gmail.com>
2024-09-25 11:26:32 +0100

    Update responsive design

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,36 @@
 import styles from "./Footer.module.css"
 
 export const Footer = () => {
+  const openPortfolio = () => {
+    window.open("https://pablocaballero1295.github.io/Portfolio/", "_blank")
+  }
+
+  const openValorantAPI = () => {
+    window.open("https://valorant-api.com/", "_blank")
+  }
+
+  const openValorantWeb = () => {
+    window.open("https://playvalorant.com/", "_blank")
+  }
+
   return (
     <div className={styles.footer}>
-      <div className="container">Footer - Work in Progress</div>
+      <div className="container">
+        <div className={styles.footer_container}>
+          <div className={styles.links_title}>ENLACES</div>
+          <div className={styles.links}>
+            <div className={styles.link_item} onClick={openValorantAPI}>
+              API
+            </div>
+            <div className={styles.link_item} onClick={openPortfolio}>
+              PORTFOLIO
+            </div>
+            <div className={styles.link_item} onClick={openValorantWeb}>
+              VALORANT
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
